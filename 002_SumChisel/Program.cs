e07c681d0c7e2ea4f3b340415b2d25474e10c3c3
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-11 21:46:23 +0300

    add 002

---
+++ 002_SumChisel/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+int a = 1;
+while(a >= 1)
+{
+    a = N % 10;
+    N = N/10;
+    sum = sum + a;
+}
+Console.WriteLine("Сумма цифр в числе:" +sum);
