db6666828260ed18393f5a30b7ab4676d2ee0f28
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-11 21:55:51 +0300

    add 003

---
+++ 003_ProizvedenieChusel/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int comp = 1;
+while(N > 0)
+{
+    comp = comp * N;
+    N = N - 1;
+}
+Console.WriteLine("Произведение чисел от 1 до N: " +comp);
