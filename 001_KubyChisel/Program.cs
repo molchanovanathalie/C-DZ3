05430541995bd1d87b661670fd7c85b115b404b5
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-11 21:15:24 +0300

    add 001

---
+++ 001_KubyChisel/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while(a<=N)
+{
+    int b = a*a*a;
+    Console.WriteLine($"a = {a}, куб числа a = {b}");
+    a++;
+}
